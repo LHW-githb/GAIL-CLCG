2c75ea7086fcfc7994fa073c578b90212c3c292f
LHW-githb <2442113217@qq.com>
2025-02-15 13:04:08 +0800

    Update gail3.py

--- gail3.py
+++ gail3.py
@@ -388,7 +388,7 @@ class GAIL3(Module):
 
             if hasattr(self, "v"):
                 torch.save(
-                    self.pi.state_dict(), f"task3_value_[{i}].ckpt"
+                    self.v.state_dict(), f"task3_value_[{i}].ckpt"
                 )
 
         return exp_rwd_mean, rwd_iter_means
